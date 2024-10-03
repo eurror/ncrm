fdc6e1043447b15922df606146ed5e73a3919b21
eurus <evr@gmx.us>
2024-10-03 20:22:47 +0500

    replaced swagger end-points

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,29 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
 
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Task Tracking API",
+        default_version='v1',
+        description="Документация для API системы управления задачами",
+    ),
+    public=True,
+    permission_classes=(AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tasks.urls')),
     path('', include('accounts.urls')),
+    path('swagger/', schema_view.with_ui('swagger',
+         cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc',
+         cache_timeout=0), name='schema-redoc'),
+]
+urlpatterns += [
+
 ]
--- tasks/urls.py
+++ tasks/urls.py
@@ -1,20 +1,8 @@
 from rest_framework.routers import DefaultRouter
 from .views import BoardViewSet, TaskViewSet, StatusViewSet, TaskHistoryViewSet
 from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+
 from django.urls import path
-
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Task Tracking API",
-        default_version='v1',
-        description="Документация для API системы управления задачами",
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
 
 router = DefaultRouter()
 router.register(r'boards', BoardViewSet)
@@ -23,7 +11,3 @@ router.register(r'statuses', StatusViewSet)
 router.register(r'task-history', TaskHistoryViewSet)
 
 urlpatterns = router.urls
-urlpatterns += [
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]
